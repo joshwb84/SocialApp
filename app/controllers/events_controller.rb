8e0c858cc11b4f3f2056533d00c4cb3634385dea
Joshua Borin <joshua.borin@gmail.com>
2015-02-12 17:22:01 -0600

    Form_for for Events view and controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,22 +11,31 @@ def show
 end
 
 def new
+@event = Event.new
 end
 
 def create
-# do something with params
+
+  Event.create(event_params)
+    redirect_to events_path
 end
 
 def edit
 # do something with params
+@event = Event.find_by(id: params["id"])
 end
 
 def update
-# do something with params
+event_params = params.require(:event).permit(:category, :title, :description, :date, :time, :location)
+@event = Event.find_by(id: params["id"])
+  @event.update(event_params)
+    redirect_to events_path
 end
 
 def destroy
-# do something with params
+  @event = Event.find_by(id: params["id"])
+    @event.destroy
+    redirect_to events_path
 end
 
 end
