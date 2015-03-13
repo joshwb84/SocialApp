28962c6333d6798a86cb32a29dc3c62a347c4c6c
Joshua Borin <joshua.borin@gmail.com>
2015-03-13 14:34:06 -0500

    reservation button fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313034840) do
+ActiveRecord::Schema.define(version: 20150313033811) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -54,15 +54,11 @@ ActiveRecord::Schema.define(version: 20150313034840) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.boolean  "tandc",               default: false
+    t.boolean  "tandc",           default: false
     t.text     "description"
     t.string   "facebook"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
 end
