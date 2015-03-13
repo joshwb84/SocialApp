c6c117850835266a42c74fdaf866deee9bd83648
Joshua Borin <joshua.borin@gmail.com>
2015-03-13 11:08:57 -0500

    modal button and S3 connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ has_many :reservations
  has_attached_file :avatar, 
  :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
  :default_url => ":style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/,  
   :storage => :s3,
   :s3_credentials => "#{Rails.root}/config/s3.yml",
   :path => ":attachment/:id/:style.:extension",
