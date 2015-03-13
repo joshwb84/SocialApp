bcbf2d081deb6cb4db4fa3c2648ace9c92900717
Joshua Borin <joshua.borin@gmail.com>
2015-03-13 10:58:47 -0500

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ has_many :reservations
  :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
  :default_url => ":style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
   :storage => :s3,
   :s3_credentials => "#{Rails.root}/config/s3.yml",
   :path => ":attachment/:id/:style.:extension",
