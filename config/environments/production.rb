81a0ab25c6286965723ff515805c035c6ee13fc4
Joshua Borin <joshua.borin@gmail.com>
2015-03-13 16:25:37 -0500

    production.rb generic change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,12 +80,12 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.assets.compile = true
 
-  config.paperclip_defaults = {
+config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['konverge'],
-    :access_key_id => ENV['AKIAJSQ7KIWPDLHTXRFA'],
-    :secret_access_key => ENV['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']
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 
